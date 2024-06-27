9cf92254af66129a4083c361375cb8284325e29a
LatifurRatul <latifursoikot@gmail.com>
2024-06-27 23:09:46 +0600

    Problem solution Commit

--- problem1.js
+++ problem1.js
@@ -3,13 +3,17 @@
 function findAverageArray(array) {
 
     let sum = 0;
+    let average = 0;
     for (let arr of array) {
         sum += arr;
     }
-    console.log(`Sum of all array is: ${sum}`);
+
+    average = (sum / (array.length));
+
+    console.log(`Sum of all array is: ${average.toFixed(2)}`);
 
 }
 
-let arr = [2, 3, 4];
+let arr = [2, 3, 5];
 
 findAverageArray(arr);
